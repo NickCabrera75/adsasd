0070e5230955d1c02781d57234e8e175f0552b53
Rene Perez Rodriguez <rene_perez@outlook.it>
2018-11-26 23:23:30 -0400

    filtro mascotas

--- application/controllers/adopcionperros.php
+++ application/controllers/adopcionperros.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Perrosgatos extends CI_Controller {
+class Adopcionperros extends CI_Controller {
   public function __construct()
   {
     parent::__construct();
@@ -8,9 +8,11 @@ class Perrosgatos extends CI_Controller {
   }
   public function index()
   {
-  $data = array('animales' => $this->animales_model->listaanimales());
-    $this->load->view('inc/template_header');
-    $this->load->view('view/perrosgatos_view',$data);
+    $listaanimales=$this->animales_model->listaanimales();
+    $data['animales']=$listaanimales;
+     $this->load->view('inc/template_headerLogueado');
+    $this->load->view('view/adopcionperro_view',$data);
     $this->load->view('inc/template_footer');
   }
+
 }
--- application/views/view/perros_view.php
+++ application/views/view/perros_view.php
@@ -14,6 +14,7 @@
 			<!-- End banner Area -->	
 				
 			<!-- Start cat-list Area -->
+
 		<?php
 
                             foreach ($animales->result() as $row)
